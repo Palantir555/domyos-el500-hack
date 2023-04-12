110b5027cea548c2d9f57a261ac05443b7cbdb06
Juan Carlos Jimenez <jcjc.dev@gmail.com>
2023-04-12 02:02:16 +0200

    working javascript dot with a trail behind it

--- custom-app/webgame/static/game.js
+++ custom-app/webgame/static/game.js
@@ -8,18 +8,46 @@ let dotX = 0;
 let dotY = canvas.height - (3*dotSize); //canvas.height / 2;
 let speed = 0;
 let slope = 0;
+pastPositionsUpdated = false;
+
+const pastDotPositions = []; // Add this line to store past dot positions
+
+function updatePastDotPositions(horizontalDisplacement, verticalDisplacement) {
+  // TODO: massively wasteful? or what?
+  for (let i = 0; i < pastDotPositions.length; i++) {
+    pastDotPositions[i].x -= horizontalDisplacement;
+    pastDotPositions[i].y += verticalDisplacement;
+  }
+  pastPositionsUpdated = true;
+}
 
 function drawDot() {
-  // Leave a trail behind the dot
-    ctx.fillStyle = "rgba(255, 255, 255, 0.3)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Clear the canvas only when necessary, e.g., when resizing
+  if (pastPositionsUpdated) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    pastPositionsUpdated = false;
+  }
+
+  // Store the current dot position
+  pastDotPositions.push({ x: dotX, y: dotY });
+
+  // Draw the trail (smaller dots) for past positions
+  ctx.fillStyle = "#dddcd8";
+  pastDotPositions.forEach((position) => {
+    ctx.beginPath();
+    ctx.arc(position.x, position.y, dotSize / 2, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+  });
+
+  // Draw the main dot
   ctx.beginPath();
   ctx.arc(dotX, dotY, dotSize, 0, 2 * Math.PI);
   ctx.fillStyle = "#dddcd8";
   ctx.fill();
   ctx.closePath();
 }
+
 
 function calculateDisplacement(speed, slope) {
 
@@ -53,13 +81,17 @@ function updateDot() {
 
   if (dotX >= (canvas.width - 3*dotSize)) {
     // Move the canvas to the left by -horizontalDisplacement
-    canvas.style.left = -horizontalDisplacement + "px";
+    canvas.style.left = horizontalDisplacement + "px";
+    // Move the dot trail
+    updatePastDotPositions(horizontalDisplacement, 0);
     // Keep the dot next to the right edge of the canvas
     dotX = canvas.width - (3*dotSize);
   }
   if (dotY <= (canvas.height / 4)) {
     // Move the canvas by verticalDisplacement
-    canvas.style.top = -verticalDisplacement + "px";
+    canvas.style.top = verticalDisplacement + "px";
+    // Move the dot trail
+    updatePastDotPositions(0, verticalDisplacement);
     // Keep the dot next to the top edge of the canvas
     dotY = (canvas.height / 4);
   }
